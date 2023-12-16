f981de9c780c8c38a9ce33f07e1898688b54eb62
Pina641 <kakinotane2048@gmail.com>
2023-12-17 00:29:00 +0900

    post

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 
 	traqwsbot "github.com/traPtitech/traq-ws-bot"
 )
@@ -17,4 +19,24 @@ func getTest(bot *traqwsbot.Bot, channelID string) {
 	}
 	simplePost(bot, channelID, res)
 
+}
+
+func postTask(bot *traqwsbot.Bot, channelID string, newTask TaskWithoutId) {
+	var dateOfNow = time.Now().Format("2006-01-02")
+	var userId = "testuser"
+	res, err := db.Exec("INSERT INTO task (user, title, description, condition_id, difficulty, created_at, updated_at, dueDate) VALUES (?,?,?,?,?,?,?,?)", userId, newTask.Title, newTask.Description, newTask.ConditionId, newTask.Difficulty, dateOfNow, dateOfNow, newTask.DueDate)
+
+	if err != nil {
+		fmt.Println(err)
+		simplePost(bot, channelID, "Datebase insert error")
+	}
+
+	taskId, err := res.LastInsertId()
+	if err != nil {
+		fmt.Println(err)
+		simpleEdit(bot, channelID, "ID get error")
+	}
+
+	resStr := "##タスクを追加しました！\n|ID|タスク名|期限|\n|---|---|---|\n|" + strconv.Itoa(int(taskId)) + "|" + newTask.Title + "|" + newTask.DueDate + "|\n"
+	simplePost(bot, channelID, resStr)
 }
