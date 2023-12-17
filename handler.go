9778bb6bd0951873a9c031e5b3efb2519711aeac
Pina641 <kakinotane2048@gmail.com>
2023-12-17 14:34:45 +0900

    help 系コマンドの実装

--- handler.go
+++ handler.go
@@ -8,18 +8,6 @@ import (
 	traqwsbot "github.com/traPtitech/traq-ws-bot"
 )
 
-func getTest(bot *traqwsbot.Bot, channelID string) {
-	var tasks []Task
-	if err := db.Select(&tasks, "SELECT * FROM task"); err != nil {
-		fmt.Println(err)
-	}
-	res := "## タスク一覧\n|タスク名|期限|\n|---|---|\n"
-	for _, v := range tasks {
-		res += "|" + v.Title + "|" + v.Description + "|\n"
-	}
-	simplePost(bot, channelID, res)
-}
-
 func getTask(bot *traqwsbot.Bot, userID string, channelID string) {
 	var tasks []Task
 	if err := db.Select(&tasks, "SELECT * FROM `task` WHERE user = ?", userID); err != nil {
